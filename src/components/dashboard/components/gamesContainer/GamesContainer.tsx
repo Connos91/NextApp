5ffaa0dd26cf3cc38cabcecf3b799e1afe9e8f3d
Connos91 <con.onisifo@gmail.com>
2024-12-08 01:13:25 +0200

    add title

--- src/components/dashboard/components/gamesContainer/GamesContainer.tsx
+++ src/components/dashboard/components/gamesContainer/GamesContainer.tsx
@@ -13,6 +13,9 @@ const GamesContainer = () => {
 
   return (
     <div className="bg-white/10 rounded-lg p-6 mt-10">
+      <h1 className="font-semibold text-xl tracking-tight mb-4">
+        Client Games
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {games?.map((game: any) => (
           <Game id={game?.id} key={uuidv4()} {...game} />
